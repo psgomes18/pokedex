e64fe8c64a43423047f272df93a3ae4e25cdee98
Paulo Sergio <paulogomes1809@gmail.com>
2021-03-15 14:43:19 -0300

    Finishing the API Fetch and adding some efects with CSS

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const poke_container = document.getElementById('poke_container')
 const pokemons_number = 150;
+const colors ={
+    fire:'#FDDFDF',
+    grass:'#DEFDE0',
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    flighting: '#E6E0D4',
+    normal: '#F5F5F5', 
+}
 
+const main_types = Object.keys(colors)
 
 const fetchPokemons = async () => {
     for (let i=1; i<=pokemons_number; i++){
@@ -9,7 +26,7 @@ const fetchPokemons = async () => {
 }
 
 const getPokemon = async id => {
-    const url = `https://pokeapi.co/api/v2/pokemon/`
+    // const url = `https://pokeapi.co/api/v2/pokemon/`
     const res = await fetch(url)
     const pokemon = await res.json()
     createPokemonCard(pokemon) 
@@ -20,13 +37,19 @@ function createPokemonCard(pokemon){
     const pokemonEl = document.createElement('div')
     pokemonEl.classList.add('pokemon')
 
+    const poke_types = pokemon.types.map(el => el.type.name)
+    const type = main_types.find(type => poke_types.indeOfx(type) > -1)
     const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
 
     const pokeInnerHTML = `
         <div class="img-container">
-            <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"
+            <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png/>"
         </div>
-        ${pokemon.id}
+        <div class="info">
+            <span class="number">${pokemon.id}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Type: <span>${type}</small>
+        </div>
     
     `
     pokemonEl.innerHTML = pokeInnerHTML
