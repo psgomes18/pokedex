381896955cd8c87477077ab1d1e9f81eb4488c2d
Paulo Sergio <paulogomes1809@gmail.com>
2021-03-15 14:15:55 -0300

    Developing the fetch API to bring the pokemon informations

---
+++ app.js
@@ -0,0 +1,35 @@
+const poke_container = document.getElementById('poke_container')
+const pokemons_number = 150;
+
+
+const fetchPokemons = async () => {
+    for (let i=1; i<=pokemons_number; i++){
+        await getPokemon(i)
+    }
+}
+
+const getPokemon = async id => {
+    const url = `https://pokeapi.co/api/v2/pokemon/`
+    const res = await fetch(url)
+    const pokemon = await res.json()
+    createPokemonCard(pokemon) 
+}
+fetchPokemons()
+
+function createPokemonCard(pokemon){
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+
+    const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1)
+
+    const pokeInnerHTML = `
+        <div class="img-container">
+            <img src=""
+        </div>
+        ${pokemon.id}
+    
+    `
+    pokemonEl.innerHTML = pokeInnerHTML
+
+    poke_container.appendChild(pokemonEl)
+}
